42a3b5506773fb9d8db7c149cc325312ccf5810d
user <user@LAPTOP-P6JDQUOH>
2023-03-22 12:21:01 +0400

    Добавьте файлы проекта.

---
+++ WebApplication4/Controllers/CalculatorController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(CalculatorModel model, string action)
+        {
+            if (action == "calculate")
+            {
+                if (ModelState.IsValid)
+                {
+                    switch (model.Operation)
+                    {
+                        case "+":
+                            model.Result = model.FirstNumber + model.SecondNumber;
+                            break;
+                        case "-":
+                            model.Result = model.FirstNumber - model.SecondNumber;
+                            break;
+                        case "*":
+                            model.Result = model.FirstNumber * model.SecondNumber;
+                            break;
+                        case "/":
+                            model.Result = model.FirstNumber / model.SecondNumber;
+                            break;
+                    }
+
+                    ViewBag.Message = "Вычисление успешно.";
+                }
+                else
+                {
+                    ViewBag.Message = "Введены неверные данные.";
+                }
+
+                return View("Index", model);
+            }
+            else if (action == "clearr")
+            {
+                // Очистить модель и сбросить сообщение ViewBag
+                model.FirstNumber2 = 0;
+                model.Operation = "+";
+
+                ViewBag.Message = "Поля очищены.";  
+
+                return View("Index", model);
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+    }
+
+}
---
+++ WebApplication4/Models/CalculatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class CalculatorModel
+    {
+        [Required(ErrorMessage = "FirstNumber является обязательным.")]
+        public sbyte FirstNumber { get; set; }
+        public sbyte FirstNumber2 { get; set; }
+
+        [Range(-128, 127, ErrorMessage = "Поле SecondNumber должно быть от -128 до 127.")]
+
+        public sbyte SecondNumber { get; set; }
+        public string Operation { get; set; }
+        public decimal Result { get; set; }
+    }
+
+}
+
